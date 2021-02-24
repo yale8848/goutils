be30dfbbcf563bf07dd22580e56a4fd57bd91b5f
yale8848 <314316537@qq.com>
2021-02-24 16:45:26 +0800

    add file func

--- pkg/files/file.go
+++ pkg/files/file.go
@@ -9,3 +9,14 @@ func MkDirs(fp string) {
 	fd := filepath.Base(fp)
 	os.MkdirAll(fd, 0666)
 }
+
+func IsExist(fpath string) (bool,error) {
+	_, err := os.Stat(fpath)
+	if err==nil {
+		return true,nil
+	}
+	if os.IsNotExist(err) {
+		return false,nil
+	}
+	return false,err
+}
