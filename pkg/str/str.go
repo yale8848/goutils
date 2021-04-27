967c4bee838660926b8795f42f2b2c072a29d6ac
yale8848 <royal8848@163.com>
2021-04-27 16:20:42 +0800

    add substr

--- pkg/str/str.go
+++ pkg/str/str.go
@@ -3,3 +3,17 @@ package str
 func IsEmpty(s string) bool {
 	return len(s) == 0
 }
+
+func SubStr(src string , start,endExclude int)string  {
+
+	runes:=[]rune(src)
+	rLen:=len(runes)
+	if start<0||endExclude>rLen||start>endExclude {
+		return ""
+	}
+	if start == 0 && endExclude == rLen {
+		return src
+	}
+	return string(runes[start:endExclude])
+
+}
