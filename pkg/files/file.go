2b9eb197a839e2658c975632f8d930004f68c196
yale8848 <royal8848@163.com>
2021-04-27 16:33:55 +0800

    add Copy

--- pkg/files/file.go
+++ pkg/files/file.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -19,4 +20,27 @@ func IsExist(fpath string) (bool,error) {
 		return false,nil
 	}
 	return false,err
+}
+func IsFileExist(fpath string) (bool) {
+	_, err := os.Stat(fpath)
+	if err==nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+func Copy(srcFile,distFile string) (err error) {
+	fs,err:=os.Open(srcFile)
+	if err!=nil {
+		return
+	}
+	fd,err:=os.Create(distFile)
+	if err!=nil {
+		return
+	}
+	_,err=io.Copy(fd,fs)
+	return
 }
