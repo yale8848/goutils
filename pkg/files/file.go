69f4250cf0f3e45146e067b62dce0103281fd43e
yale8848 <royal8848@163.com>
2022-02-01 10:56:48 +0800

    update

--- pkg/files/file.go
+++ pkg/files/file.go
@@ -73,3 +73,13 @@ func Copy(srcFile, distFile string, isDeleteSrc bool) (err error) {
 func DeleteFile(f string) error {
 	return os.RemoveAll(f)
 }
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
