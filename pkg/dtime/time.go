71e5c351eaacc098e668ba0b40d31c7b9c366214
yale8848 <royal8848@163.com>
2022-03-24 14:44:13 +0800

    update

--- pkg/dtime/time.go
+++ pkg/dtime/time.go
@@ -15,3 +15,13 @@ func TimeIntToDate(time_int int) string {
 	var cstZone = time.FixedZone(zone, 8*3600)
 	return time.Unix(int64(time_int), 0).In(cstZone).Format("2006-01-02 15:04:05")
 }
+
+func GetNowDateTime() string {
+	var cstZone = time.FixedZone(zone, 8*3600)
+	return time.Now().In(cstZone).Format("2006-01-02 15:04:05")
+}
+
+func GetDate() string {
+	var cstZone = time.FixedZone(zone, 8*3600)
+	return time.Now().In(cstZone).Format("2006-01-02")
+}
