ecc2b11d4748b2c81a7086048cffbc52ed6aadfd
yale8848 <royal8848@163.com>
2022-12-18 21:15:59 +0800

    udpate

---
+++ pkg/check/check.go
@@ -0,0 +1,18 @@
+package check
+
+import "regexp"
+
+func CheckIsMobile(mobileNum string) bool {
+	var regular = "^1[345789]{1}\\d{9}$"
+	reg := regexp.MustCompile(regular)
+	return reg.MatchString(mobileNum)
+}
+
+//判断是否是18或15位身份证
+func IsIdCard(cardNo string) bool {
+	//18位身份证 ^(\d{17})([0-9]|X)$
+	if m, _ := regexp.MatchString(`(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)`, cardNo); !m {
+		return false
+	}
+	return true
+}
