9a8c741858a6e4054ef2f5bcde12243c05e4873f
yale8848 <royal8848@163.com>
2023-01-04 10:50:14 +0800

    update

--- pkg/conv/conv.go
+++ pkg/conv/conv.go
@@ -1,5 +1,12 @@
 package conv
 
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
+
 func cloneBytes(b []byte) []byte {
 	if b == nil {
 		return nil
@@ -8,4 +15,32 @@ func cloneBytes(b []byte) []byte {
 		copy(c, b)
 		return c
 	}
+}
+func AsString(src interface{}) string {
+	switch v := src.(type) {
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	case int:
+		return strconv.Itoa(v)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 64)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case time.Time:
+		return time.Time.Format(v, "2006-01-02 15:04:05")
+	case bool:
+		return strconv.FormatBool(v)
+	default:
+		{
+			b, _ := json.Marshal(v)
+			return string(b)
+		}
+	}
+	return fmt.Sprintf("%v", src)
 }
